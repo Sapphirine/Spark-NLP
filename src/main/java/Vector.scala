58dc7cc972ff35b773206dcd3012dc5d39ec6a68
pharnoux <pa2398@columbia.edu>
2014-12-08 14:54:00 -0500

    vector

---
+++ src/main/java/Vector.scala
@@ -0,0 +1,24 @@
+class Vector(val vect:Array[Int]) {
+    var data:Array[Int]=vect;
+  def this(size:Int){
+    this(new Array[Int](size));
+  }
+
+  def increment(index:Int){
+    data(index)+=1;
+  }
+  def decrement(index:Int){
+    data(index)-=1;
+  }
+  def printIt(){
+    print("[")
+    for(i<-0 to data.length-1)print(data(i)+",");
+        print("]\n")
+  }
+  def forEach(callback:(Int) => Unit)={
+    for(i<-0 to data.length-1)callback(data(i));
+  }
+  def add(a:Array[Int]){
+    for(i<-0 to data.length-1)data(i)+=a(i);
+  }
+  }
