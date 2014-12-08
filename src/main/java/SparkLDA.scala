5e2d6dd4fcc95964b3cd5ca1b1dffe8bd2bd45ce
nerajbobra <nbobra@gmail.com>
2014-12-07 21:12:20 -0500

    initial commit

--- src/main/java/SparkLDA.scala
+++ src/main/java/SparkLDA.scala
@@ -1,14 +1,15 @@
 import org.apache.spark.SparkContext
 import org.apache.spark.SparkContext._
 import org.apache.spark.SparkConf
-import java.util.Map
-import java.util.HashMap
+import scala.collection.mutable._
+import org.apache.commons.compress.archivers.zip.AsiExtraField
+
 
 
 object SparkLDA {
 
 	def main(args: Array[String]) {
-		importText("hdfs://localhost:8020/README.md");
+		importText("hdfs://localhost:8020/README.md",10);
 	}
 
 	def lda(pathToFileIn:String,pathToFileOut:String,pathEvalLabels:String,numTopics:Int,alpha:Double,beta:Double,numIter:Int,deBug:Boolean){
@@ -20,13 +21,20 @@ object SparkLDA {
 		(conf,sc)
 	}
 
-	def importText(pathToFileIn:String){//,sc:SparkContext){
+	def importText(pathToFileIn:String,numTopics:Int){//,sc:SparkContext){
 		val conf = new SparkConf().setAppName("Simple Application")
 				val sc = new SparkContext(conf)
 		val stopWords =List[String]("a","able","about","above","according","accordingly","across","actually","after","afterwards","again","against","all","allow","allows","almost","alone","along","already","also","although","always","am","among","amongst","an","and","another","any","anybody","anyhow","anyone","anything","anyway","anyways","anywhere","apart","appear","appreciate","appropriate","are","around","as","aside","ask","asking","associated","at","available","away","awfully","b","be","became","because","become","becomes","becoming","been","before","beforehand","behind","being","believe","below","beside","besides","best","better","between","beyond","both","brief","but","by","c","came","can","cannot","cant","cause","causes","certain","certainly","changes","clearly","co","com","come","comes","concerning","consequently","consider","considering","contain","containing","contains","corresponding","could","course","currently","d","definitely","described","despite","did","different","do","does","doing","done","down","downwards","during","e","each","edu","eg","eight","either","else","elsewhere","enough","entirely","especially","et","etc","even","ever","every","everybody","everyone","everything","everywhere","ex","exactly","example","except","f","far","few","fifth","first","five","followed","following","follows","for","former","formerly","forth","four","from","further","furthermore","g","get","gets","getting","given","gives","go","goes","going","gone","got","gotten","greetings","h","had","happens","hardly","has","have","having","he","hello","help","hence","her","here","hereafter","hereby","herein","hereupon","hers","herself","hi","him","himself","his","hither","hopefully","how","howbeit","however","i","ie","if","ignored","immediate","in","inasmuch","inc","indeed","indicate","indicated","indicates","inner","insofar","instead","into","inward","is","it","its","itself","j","just","k","keep","keeps","kept","know","knows","known","l","last","lately","later","latter","latterly","least","less","lest","let","like","liked","likely","little","ll","look","looking","looks","ltd","m","mainly","many","may","maybe","me","mean","meanwhile","merely","might","more","moreover","most","mostly","much","must","my","myself","n","name","namely","nd","near","nearly","necessary","need","needs","neither","never","nevertheless","new","next","nine","no","nobody","non","none","noone","nor","normally","not","nothing","novel","now","nowhere","o","obviously","of","off","often","oh","ok","okay","old","on","once","one","ones","only","onto","or","other","others","otherwise","ought","our","ours","ourselves","out","outside","over","overall","own","p","particular","particularly","per","perhaps","placed","please","plus","possible","presumably","probably","provides","q","que","quite","qv","r","rather","rd","re","really","reasonably","regarding","regardless","regards","relatively","respectively","right","s","said","same","saw","say","saying","says","second","secondly","see","seeing","seem","seemed","seeming","seems","seen","self","selves","sensible","sent","serious","seriously","seven","several","shall","she","should","since","six","so","some","somebody","somehow","someone","something","sometime","sometimes","somewhat","somewhere","soon","sorry","specified","specify","specifying","still","sub","such","sup","sure","t","take","taken","tell","tends","th","than","thank","thanks","thanx","that","thats","the","their","theirs","them","themselves","then","thence","there","thereafter","thereby","therefore","therein","theres","thereupon","these","they","think","third","this","thorough","thoroughly","those","though","three","through","throughout","thru","thus","to","together","too","took","toward","towards","tried","tries","truly","try","trying","twice","two","u","un","under","unfortunately","unless","unlikely","until","unto","up","upon","us","use","used","useful","uses","using","usually","uucp","v","value","various","ve","very","via","viz","vs","w","want","wants","was","way","we","welcome","well","went","were","what","whatever","when","whence","whenever","where","whereafter","whereas","whereby","wherein","whereupon","wherever","whether","which","while","whither","who","whoever","whole","whom","whose","why","will","willing","wish","with","within","without","wonder","would","would","x","y","yes","yet","you","your","yours","yourself","yourselves","z","zero");
 		val textFile=sc.textFile(pathToFileIn);
 		var index=0;
 		var dictionnary:scala.collection.mutable.Map[String,Int] = scala.collection.mutable.Map[String,Int]();
+
+    var wordToTopics:scala.collection.mutable.Map[String, Array[Int]] = new HashMap[String, Array[Int]]();
+
+
+
+
+
 		//		val documents=textFile.map(line=>{
 		//			index=index+1;
 		//      println("in")
@@ -42,20 +50,72 @@ object SparkLDA {
 		//				}
 		//			}))
 		//		});
+
+		
+
 		val documents=textFile.map(line=>{
 			index=index+1;
 			val lineCleaned=line.replaceAll("[^A-Za-z ]","").toLowerCase();
 			(index,lineCleaned.split(" ").map(word=>{
-        dictionnary.put(word,0);
+				dictionnary.put(word,0);
 				if(word.length()>1&&(!stopWords.contains(word))){
-          
-					(word,1)
+					var randNum = new scala.util.Random
+							(word,randNum.nextInt(numTopics-1))
 				}
 			}))
-		}).foreach(t=>t._2.foreach(println));
+		})
+    
+    documents.foreach(
+				t=>t._2.foreach{f=> {
+					f match{
+
+					case (word, topic) => {
+            val currentArray:Array[Int] = wordToTopics.getOrElse(word.toString(),new Array[Int](numTopics));
+            
+            //print the current array
+            
+            for ( x <- 0 to numTopics-1 ) {
+              print(currentArray(x) + " ")
+            }
+            println()
+            
+            
+            currentArray(topic.asInstanceOf[Int])+=1;
+            wordToTopics.put(word.toString(), currentArray)
+          }
+          case _ =>
+            
+					}
+				}
+
+
+				/*
+            val key = f.
+            var value = t._2
+
+            var currentList = wordToTopics.get(key);
+            var newList = Array(1, 2, 3)
+            var oldList = wordToTopics.put("string", newList)
+				 * 
+				 */
+				
+          
+        }
+        
+
+
+				);
+    
+    println(wordToTopics.isEmpty);
+
 
 		println("size of dictionnary :"+dictionnary.size+" size file :"+textFile.count())
 		(documents,dictionnary)
+
+
+		//val temp:scala.collection.mutable.LinkedList[Int] = wordToTopics.get(t._1)
+		//Map.put(t._1, )
+
 	}
 	//	def initializeTopicDistribution(documents:RDD[String]){
 	//		/*
